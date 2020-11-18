64d44d8a9e04c3c2e030d1b3085a557488d5228d
Saurav Adhikari <adhikarisaurav368@gmail.com>
2020-11-18 12:11:56 +0545

    Updates

---
+++ precache-manifest.e2e43d7e7505f1d8ad554c3ea0e2072f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e583ffd2591c46b04358e96d604a6f",
+    "url": "/aveApp/index.html"
+  },
+  {
+    "revision": "6210ade178f3fe009169",
+    "url": "/aveApp/static/css/main.e747ae69.chunk.css"
+  },
+  {
+    "revision": "0fe30deac3deb0fec6dc",
+    "url": "/aveApp/static/js/2.238117a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/aveApp/static/js/2.238117a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6210ade178f3fe009169",
+    "url": "/aveApp/static/js/main.02254e64.chunk.js"
+  },
+  {
+    "revision": "9efa16bb27b5a11fe6bb",
+    "url": "/aveApp/static/js/runtime-main.cc5a794b.js"
+  },
+  {
+    "revision": "1a9020c337be1b3a6556473307bb9a9f",
+    "url": "/aveApp/static/media/envelope-regular.1a9020c3.svg"
+  },
+  {
+    "revision": "53757d9306796cd8c19df677d30a10c7",
+    "url": "/aveApp/static/media/facebook-f-brands.53757d93.svg"
+  },
+  {
+    "revision": "276f52a709627c3d36a273e940fc93b2",
+    "url": "/aveApp/static/media/google-plus-g-brands.276f52a7.svg"
+  },
+  {
+    "revision": "e75317fafc5e835195adf92be513d35b",
+    "url": "/aveApp/static/media/instagram-brands.e75317fa.svg"
+  },
+  {
+    "revision": "b216cb024e56e3a615b2b63e444aa1ea",
+    "url": "/aveApp/static/media/pinterest-p-brands.b216cb02.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/aveApp/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "6620c8ae2cac90067a9aa78339bb808d",
+    "url": "/aveApp/static/media/twitter-brands.6620c8ae.svg"
+  }
+]);
